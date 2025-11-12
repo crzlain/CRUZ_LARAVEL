921d1e35198e883d6ecdc1079a27ddb90508898c
raine <lorraineanncruz00@gmail.com>
2025-11-13 00:21:24 +0800

    Updated web.php, added index and portfolio views, updated assets

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('index');
+});
+Route::get('portfolio', function () {
+    return view('portfolio');
+});
+Route::get('welcome', function () {
     return view('welcome');
 });
